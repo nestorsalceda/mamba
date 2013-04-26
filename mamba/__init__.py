7278f68b18f8cee3f9a78e1265df0994a23254bc
Néstor Salceda <nestor.salceda@gmail.com>
2013-04-26 11:36:46 +0200

    Fix the import all mamba error

--- mamba/__init__.py
+++ mamba/__init__.py
@@ -2,4 +2,4 @@ from mamba.loader import describe, context
 from mamba.hooks import before, after
 from mamba.decorators import skip
 
-__all__ = [describe, context, before, after, skip]
+__all__ = ['describe', 'context', 'before', 'after', 'skip']
