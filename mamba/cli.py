75add42972c059a00b01fe2b4eeb716d905a3bd6
Néstor Salceda <nestor.salceda@gmail.com>
2013-04-29 23:51:26 +0200

    Load specs from spec/**/*_spec.py if no spec was specified

--- mamba/cli.py
+++ mamba/cli.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import sys
+import os
 import imp
 
 from mamba import formatters
@@ -11,7 +12,7 @@ def main():
     formatter = formatters.DocumentationFormatter()
     runner = Runner(formatter)
 
-    for file_ in sys.argv[1:]:
+    for file_ in _specs():
         module = imp.load_source(file_.replace('.py', ''), file_)
         runner.run(module)
 
@@ -19,6 +20,17 @@ def main():
 
     if runner.has_failed_specs:
         sys.exit(1)
+
+
+def _specs():
+    if len(sys.argv) == 1:
+        collected = []
+        for root, dirs, files in os.walk('spec'):
+            collected.extend([os.path.join(root, file_) for file_ in files if file_.endswith('_spec.py')])
+        collected.sort()
+        return collected
+    else:
+        return sys.argv[1:]
 
 
 if __name__ == '__main__':
