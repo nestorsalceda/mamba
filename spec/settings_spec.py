1e3109f154ab86273996e4b598cea706c766cb8b
Néstor Salceda <nestor.salceda@gmail.com>
2013-05-03 13:14:31 +0200

    Use subject for test settings

--- spec/settings_spec.py
+++ spec/settings_spec.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from mamba import describe, context, before
+from mamba import describe, context
 from sure import expect
 
 from mamba.settings import Settings
@@ -8,17 +8,13 @@ from mamba.settings import Settings
 IRRELEVANT_SLOW_TEST_THRESHOLD = '0.1'
 
 
-with describe('Settings') as _:
-    @before.each
-    def create_settings():
-        _.settings = Settings()
-
+with describe(Settings) as _:
     with context('when loading defaults'):
         def it_should_have_75_millis_as_slow_test_threshold():
-            expect(_.settings).to.have.property('slow_test_threshold').to.be.equal(0.075)
+            expect(_.subject).to.have.property('slow_test_threshold').to.be.equal(0.075)
 
     with context('when setting custom values'):
         def it_should_set_slow_test_threshold():
-            _.settings.slow_test_threshold = IRRELEVANT_SLOW_TEST_THRESHOLD
+            _.subject.slow_test_threshold = IRRELEVANT_SLOW_TEST_THRESHOLD
 
-            expect(_.settings).to.have.property('slow_test_threshold').to.be.equal(IRRELEVANT_SLOW_TEST_THRESHOLD)
+            expect(_.subject).to.have.property('slow_test_threshold').to.be.equal(IRRELEVANT_SLOW_TEST_THRESHOLD)
