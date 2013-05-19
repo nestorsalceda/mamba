a89b73ce0b049104a44504c89ce942509ba22d8f
Néstor Salceda <nestor.salceda@gmail.com>
2013-05-19 20:19:07 +0200

    Show all failures with stacktraces at the end of summary

--- mamba/formatters.py
+++ mamba/formatters.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 
+import traceback
+
 from clint.textui import indent, puts, colored
 from mamba import spec
 
@@ -9,13 +11,9 @@ class DocumentationFormatter(object):
     def __init__(self, settings):
         self.settings = settings
         self.total_specs = 0
-        self.failed_specs = 0
+        self.failed_specs = []
         self.skipped_specs = 0
         self.total_seconds = .0
-
-    @property
-    def has_failed_specs(self):
-        return self.failed_specs != 0
 
     @property
     def has_skipped_specs(self):
@@ -52,12 +50,12 @@ class DocumentationFormatter(object):
             symbol = colored.green('✓')
             if spec_.failed:
                 symbol = colored.red('✗')
-                self.failed_specs += 1
+                self.failed_specs.append(spec_)
             elif spec_.skipped:
                 symbol = colored.yellow('✗')
                 self.skipped_specs += 1
 
-            puts(symbol + ' ' + spec_.name.replace('_', ' ') + self.format_slow_test(spec_))
+            puts(symbol + ' ' + self.format_spec_name(spec_) + self.format_slow_test(spec_))
 
             if spec_.failed:
                 with indent(spec_.depth + 2):
@@ -65,14 +63,52 @@ class DocumentationFormatter(object):
 
         self.total_specs += 1
 
+    def format_spec_name(self, spec_):
+        return spec_.name.replace('_', ' ')
+
     def format_summary(self):
         puts()
-        if self.has_failed_specs:
-            puts(colored.red("%d specs failed of %d ran in %s" % (self.failed_specs, self.specs_ran, self.format_seconds(self.total_seconds))))
+        if self.failed_specs:
+            self.format_failed_specs()
+            puts(colored.red("%d specs failed of %d ran in %s" % (len(self.failed_specs), self.specs_ran, self.format_seconds(self.total_seconds))))
         elif self.has_skipped_specs:
             puts(colored.yellow("%d specs ran (%d skipped) in %s" % (self.specs_ran, self.skipped_specs, self.format_seconds(self.total_seconds))))
         else:
             puts(colored.green("%d specs ran in %s" % (self.specs_ran, self.format_seconds(self.total_seconds))))
+
+    def format_failed_specs(self):
+        puts('Failures:')
+        puts()
+        with indent(2):
+            for index, failed in enumerate(self.failed_specs):
+                puts('%d) %s' % (index + 1, self.format_full_spec_name(failed)))
+                with indent(3):
+                    puts(colored.red('Failure/Error: %s' % self.format_failing_expectation(failed)))
+                    puts()
+                    puts(colored.red(str(failed.exception)))
+                    puts()
+                    puts('Traceback:')
+                    puts(colored.red(self.format_traceback(failed)))
+                    puts()
+
+
+    def format_full_spec_name(self, spec_):
+        result = [self.format_spec_name(spec_)]
+
+        current = spec_
+        while current.parent:
+            result.append(self.format_spec_name(current.parent))
+            current = current.parent
+
+        result.reverse()
+        return ' '.join(result)
+
+    def format_failing_expectation(self, spec_):
+        error = traceback.format_tb(spec_.traceback)[1:2][0]
+        return error.split('\n')[-2].strip()
+
+    def format_traceback(self, spec_):
+        return ''.join(traceback.format_tb(spec_.traceback)[1:])
 
     def format_seconds(self, seconds):
         return '%.4f seconds' % seconds
