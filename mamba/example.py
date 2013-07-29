dec260590b31a7a06f4ddc6313e9068346122e9f
Néstor Salceda <nestor.salceda@gmail.com>
2013-07-29 13:47:17 +0200

    Copy Spec to example
    I want to remove different names test/spec and change them to example

---
+++ mamba/example.py
@@ -0,0 +1,103 @@
+# -*- coding: utf-8 -*-
+
+import sys
+from datetime import datetime, timedelta
+
+
+class Example(object):
+
+    def __init__(self, test, parent=None, pending=False):
+        self.test = test
+        self.parent = parent
+        self.pending = pending
+        self._exception = None
+        self._traceback = None
+        self._elapsed_time = timedelta(0)
+
+    def run(self, reporter):
+        reporter.spec_started(self)
+        try:
+            self._begin = datetime.utcnow()
+            if self.pending:
+                reporter.spec_pending(self)
+            else:
+                self._run_inner_test(reporter)
+        except Exception as exception:
+            self._set_exception_from_inner_test()
+            self._elapsed_time = datetime.utcnow() - self._begin
+            reporter.spec_failed(self)
+
+    def _run_inner_test(self, reporter):
+        self.run_hook('before_each')
+        self.test()
+        self.run_hook('after_each')
+        self._elapsed_time = datetime.utcnow() - self._begin
+        reporter.spec_passed(self)
+
+    def _set_exception_from_inner_test(self):
+        type_, value, traceback = sys.exc_info()
+        self.exception = value
+        self.traceback = traceback
+
+    def run_hook(self, hook):
+        for parent in self._parents:
+            parent.run_hook(hook)
+
+    @property
+    def _parents(self):
+        parents = []
+        parent = self.parent
+        while parent:
+            parents.append(parent)
+            parent = parent.parent
+
+        return reversed(parents)
+
+    @property
+    def elapsed_time(self):
+        return self._elapsed_time
+
+    @property
+    def name(self):
+        return self.test.__name__
+
+    @property
+    def source_line(self):
+        return inspect.getsourcelines(self.test)[1]
+
+    @property
+    def failed(self):
+        return self.exception is not None
+
+    @property
+    def pending(self):
+        if self.parent:
+            return self._pending or self.parent.pending
+        return self._pending
+
+    @pending.setter
+    def pending(self, value):
+        self._pending = value
+
+    @property
+    def exception(self):
+        return self._exception
+
+    @exception.setter
+    def exception(self, value):
+        self._exception = value
+
+    @property
+    def traceback(self):
+        return self._traceback
+
+    @traceback.setter
+    def traceback(self, value):
+        self._traceback = value
+
+    @property
+    def depth(self):
+        if self.parent is None:
+            return 0
+
+        return self.parent.depth + 1
---
+++ spec/example_spec.py
@@ -0,0 +1,76 @@
+from mamba import describe, context, before
+from sure import expect
+from doublex import *
+
+from mamba import reporter
+from mamba.example import Example
+
+
+with describe(Example) as _:
+
+    @before.each
+    def create_new_example_and_reporter():
+        def _test():
+            _.was_run = True
+
+        _.was_run = False
+        _.example = Example(_test)
+        _.reporter = Spy(reporter.Reporter)
+
+    def it_should_have_same_name_than_inner_callable():
+        expect(_.example.name).to.be.equals(_.example.test.__name__)
+
+    def it_should_have_depth_zero_without_parent():
+        expect(_.example.depth).to.be.equal(0)
+
+    with context('when pending'):
+        @before.each
+        def run_pending_example():
+            _.example.pending = True
+            _.example.run(_.reporter)
+
+        def it_should_not_run_the_example():
+            expect(_.was_run).to.be.false
+
+        def it_notifies_is_pending():
+            assert_that(_.reporter.spec_pending, called().with_args(_.example))
+
+    with context('when run'):
+        @before.each
+        def run_example():
+            _.example.run(_.reporter)
+
+        def it_should_run_the_example():
+            expect(_.was_run).to.be.true
+
+        def it_should_calculate_elapsed_time():
+            expect(_.example.elapsed_time.total_seconds()).to.be.greater_than(0)
+
+        def it_notifies_is_started():
+            assert_that(_.reporter.spec_started, called().with_args(_.example))
+
+        def it_notifies_is_passed():
+            assert_that(_.reporter.spec_passed, called().with_args(_.example))
+
+    with context('when run failed'):
+        @before.each
+        def create_and_run_failing_example():
+            def _failing_test():
+                raise ValueError()
+
+            _.example = Example(_failing_test)
+
+            _.example.run(_.reporter)
+
+        def it_should_be_marked_as_failed():
+            expect(_.example.failed).to.be.true
+
+        def it_should_keep_the_error_if_example_failed():
+            expect(_.example.exception).to.not_be.none
+
+        def it_should_keep_the_traceback():
+            expect(_.example.traceback).to.not_be.none
+
+        def it_notifies_is_failed():
+            assert_that(_.reporter.spec_failed, called().with_args(_.example))
+
