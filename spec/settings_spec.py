f12209d687b9890a0c46200d6df423468546c4e6
Néstor Salceda <nestor.salceda@gmail.com>
2014-04-23 12:29:23 +0200

    Migrate settings spec
    I still have to implement the subject feature

--- spec/settings_spec.py
+++ spec/settings_spec.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from sure import expect
+from expects import expect
 
 from mamba.settings import Settings
 
@@ -9,29 +9,33 @@ IRRELEVANT_ENABLE_CODE_COVERAGE = 'irrelevant enable code coverage'
 IRRELEVANT_ENABLE_FILE_WATCHER = 'irrelevant enable file watcher'
 
 
-with _description(Settings) as _:
+with description(Settings):
+
+    with before('each'):
+        self.subject = Settings()
+
     with context('when loading defaults'):
-        def it_has_75_millis_as_slow_test_threshold():
-            expect(_.subject).to.have.property('slow_test_threshold').to.be.equal(0.075)
+        with it('has 75 millis as slow test threshold'):
+            expect(self.subject).to.have.property('slow_test_threshold').to.be.equal(0.075)
 
-        def it_has_code_coverage_disabled_by_default():
-            expect(_.subject).to.have.property('enable_code_coverage').to.be.false
+        with it('has code coverage disabled by default'):
+            expect(self.subject).to.have.property('enable_code_coverage').to.be.false
 
-        def it_has_file_watcher_disabled_by_default():
-            expect(_.subject).to.have.property('enable_file_watcher').to.be.false
+        with it('has file watcher disabled by default'):
+            expect(self.subject).to.have.property('enable_file_watcher').to.be.false
 
     with context('when setting custom values'):
-        def it_sets_slow_test_threshold():
-            _.subject.slow_test_threshold = IRRELEVANT_SLOW_TEST_THRESHOLD
+        with it('sets slow test threshold'):
+            self.subject.slow_test_threshold = IRRELEVANT_SLOW_TEST_THRESHOLD
 
-            expect(_.subject).to.have.property('slow_test_threshold').to.be.equal(IRRELEVANT_SLOW_TEST_THRESHOLD)
+            expect(self.subject).to.have.property('slow_test_threshold').to.be.equal(IRRELEVANT_SLOW_TEST_THRESHOLD)
 
-        def it_sets_code_coverage():
-            _.subject.enable_code_coverage = IRRELEVANT_ENABLE_CODE_COVERAGE
+        with it('sets code coverage'):
+            self.subject.enable_code_coverage = IRRELEVANT_ENABLE_CODE_COVERAGE
 
-            expect(_.subject).to.have.property('enable_code_coverage').to.be.equal(IRRELEVANT_ENABLE_CODE_COVERAGE)
+            expect(self.subject).to.have.property('enable_code_coverage').to.be.equal(IRRELEVANT_ENABLE_CODE_COVERAGE)
 
-        def it_sets_file_watcher():
-            _.subject.enable_file_watcher = IRRELEVANT_ENABLE_FILE_WATCHER
+        with it('sets file watcher'):
+            self.subject.enable_file_watcher = IRRELEVANT_ENABLE_FILE_WATCHER
 
-            expect(_.subject).to.have.property('enable_file_watcher').to.be.equal(IRRELEVANT_ENABLE_FILE_WATCHER)
+            expect(self.subject).to.have.property('enable_file_watcher').to.be.equal(IRRELEVANT_ENABLE_FILE_WATCHER)
