e403e6c3cb0a54cdf9ce68b558c53806141f4bc2
Néstor Salceda <nestor.salceda@gmail.com>
2014-04-29 19:08:59 +0200

    Extract a description_name method

--- mamba/nodetransformers.py
+++ mamba/nodetransformers.py
@@ -22,15 +22,28 @@ class TransformToSpecsNodeTransformer(ast.NodeTransformer):
         return node
 
     def _transform_to_example_group(self, node, name):
-        description_name = self._subject(node)
-        if name in self.PENDING_EXAMPLE_GROUPS:
-            description_name += '__pending'
-        description_name += '__description'
-
-        if not isinstance(node.context_expr.args[0], ast.Str):
+        if self._subject_is_a_class(node):
             node.body.insert(0, ast.Assign(targets=[ast.Name(id='_subject_class', ctx=ast.Store())], value=node.context_expr.args[0]))
 
-        return ast.copy_location(ast.ClassDef(name=description_name, bases=[], body=node.body, decorator_list=[]), node)
+        return ast.copy_location(ast.ClassDef(name=self._description_name(node, name), bases=[], body=node.body, decorator_list=[]), node)
+
+    def _description_name(self, node, name):
+        if isinstance(node.context_expr.args[0], ast.Str):
+            description_name = node.context_expr.args[0].s
+        elif isinstance(node.context_expr.args[0], ast.Attribute):
+            description_name = node.context_expr.args[0].attr
+        else:
+            description_name = node.context_expr.args[0].id
+
+        if name in self.PENDING_EXAMPLE_GROUPS:
+            description_name += '__pending'
+
+        description_name += '__description'
+
+        return description_name
+
+    def _subject_is_a_class(self, node):
+        return not isinstance(node.context_expr.args[0], ast.Str)
 
     def _transform_to_example(self, node, name):
         example = node.context_expr.args[0].s
@@ -39,12 +52,4 @@ class TransformToSpecsNodeTransformer(ast.NodeTransformer):
     def _transform_to_hook(self, node, name):
         when = node.context_expr.args[0].s
         return ast.copy_location(ast.FunctionDef(name=name + '_' + when, args=ast.arguments(args=[ast.Name(id='self', ctx=ast.Param())], vararg=None, kwarg=None, defaults=[]), body=node.body, decorator_list=[]), node)
-
-
-    def _subject(self, node):
-        if isinstance(node.context_expr.args[0], ast.Str):
-            return node.context_expr.args[0].s
-        if isinstance(node.context_expr.args[0], ast.Attribute):
-            return node.context_expr.args[0].attr
-        return node.context_expr.args[0].id
 
