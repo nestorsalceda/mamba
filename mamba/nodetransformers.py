23be22de37599938f0263cd206b972b223641562
Néstor Salceda <nestor.salceda@gmail.com>
2014-05-13 09:11:43 +0200

    Remove duplicated code in nodetransformers

--- mamba/nodetransformers.py
+++ mamba/nodetransformers.py
@@ -22,83 +22,25 @@ class TransformToSpecsNodeTransformer(ast.NodeTransformer):
         return node
 
     def _get_name(self, node):
-        context_expr = node.context_expr
+        context_expr = self._context_expr_for(node)
 
         if isinstance(context_expr, ast.Attribute):
             return context_expr.value.id
 
         return context_expr.func.id
+
+    def _context_expr_for(self, node):
+        return node.context_expr
 
     def _transform_to_example_group(self, node, name):
+        context_expr = self._context_expr_for(node)
         if self._subject_is_a_class(node):
-            node.body.insert(0, ast.Assign(targets=[ast.Name(id='_subject_class', ctx=ast.Store())], value=node.context_expr.args[0]))
+            node.body.insert(0, ast.Assign(targets=[ast.Name(id='_subject_class', ctx=ast.Store())], value=context_expr.args[0]))
 
-        return ast.copy_location(ast.ClassDef(name=self._description_name(node, name), bases=[], body=node.body, decorator_list=[]), node)
-
-    def _description_name(self, node, name):
-        if isinstance(node.context_expr.args[0], ast.Str):
-            description_name = node.context_expr.args[0].s
-        elif isinstance(node.context_expr.args[0], ast.Attribute):
-            description_name = node.context_expr.args[0].attr
-        else:
-            description_name = node.context_expr.args[0].id
-
-        if name in self.PENDING_EXAMPLE_GROUPS:
-            description_name += '__pending'
-
-        description_name += '__description'
-
-        return description_name
-
-    def _subject_is_a_class(self, node):
-        return not isinstance(node.context_expr.args[0], ast.Str)
-
-    def _transform_to_example(self, node, name):
-        example = node.context_expr.args[0].s
-        return ast.copy_location(ast.FunctionDef(name=name + ' ' + example, args=ast.arguments(args=[ast.Name(id='self', ctx=ast.Param())], vararg=None, kwarg=None, defaults=[]), body=node.body, decorator_list=[]), node)
-
-    def _transform_to_hook(self, node, name):
-        when = node.context_expr.attr
-        return ast.copy_location(ast.FunctionDef(name=name + '_' + when, args=ast.arguments(args=[ast.Name(id='self', ctx=ast.Param())], vararg=None, kwarg=None, defaults=[]), body=node.body, decorator_list=[]), node)
-
-
-class TransformToSpecsPython3NodeTransformer(TransformToSpecsNodeTransformer):
-
-    def _get_name(self, node):
-        context_expr = node.items[0].context_expr
-
-        if isinstance(context_expr, ast.Attribute):
-            return context_expr.value.id
-
-        return context_expr.func.id
-
-    def _transform_to_example(self, node, name):
-        example = node.items[0].context_expr.args[0].s
-        return ast.copy_location(
-            ast.FunctionDef(
-                name=name + ' ' + example,
-                args=self._generate_self(),
-                body=node.body,
-                decorator_list=[]
-            ),
-            node
-        )
-
-    def _generate_self(self):
-        return ast.arguments(
-            args=[ast.arg(arg='self', annotation=None)],
-            vararg=None,
-            kwonlyargs=[],
-            kw_defaults=[],
-            kwarg=None,
-            defaults=[]
-        )
-
-    def _subject_is_a_class(self, node):
-        return not isinstance(node.items[0].context_expr.args[0], ast.Str)
+        return ast.copy_location(ast.ClassDef(name=self._description_name(node, name), bases=[], keywords=[], body=node.body, decorator_list=[]), node)
 
     def _description_name(self, node, name):
-        context_expr = node.items[0].context_expr
+        context_expr = self._context_expr_for(node)
         if isinstance(context_expr.args[0], ast.Str):
             description_name = context_expr.args[0].s
         elif isinstance(context_expr.args[0], ast.Attribute):
@@ -113,14 +55,26 @@ class TransformToSpecsPython3NodeTransformer(TransformToSpecsNodeTransformer):
 
         return description_name
 
-    def _transform_to_example_group(self, node, name):
-        if self._subject_is_a_class(node):
-            node.body.insert(0, ast.Assign(targets=[ast.Name(id='_subject_class', ctx=ast.Store())], value=node.items[0].context_expr.args[0]))
+    def _subject_is_a_class(self, node):
+        return not isinstance(self._context_expr_for(node).args[0], ast.Str)
 
-        return ast.copy_location(ast.ClassDef(name=self._description_name(node, name), bases=[], keywords=[], body=node.body, decorator_list=[]), node)
+    def _transform_to_example(self, node, name):
+        example = self._context_expr_for(node).args[0].s
+        return ast.copy_location(
+            ast.FunctionDef(
+                name=name + ' ' + example,
+                args=self._generate_self(),
+                body=node.body,
+                decorator_list=[]
+            ),
+            node
+        )
+
+    def _generate_self(self):
+        return ast.arguments(args=[ast.Name(id='self', ctx=ast.Param())], vararg=None, kwarg=None, defaults=[])
 
     def _transform_to_hook(self, node, name):
-        when = node.items[0].context_expr.attr
+        when = self._context_expr_for(node).attr
         return ast.copy_location(
             ast.FunctionDef(
                 name=name + '_' + when,
@@ -129,5 +83,21 @@ class TransformToSpecsPython3NodeTransformer(TransformToSpecsNodeTransformer):
                 decorator_list=[]
             ),
             node
+        )
+
+
+class TransformToSpecsPython3NodeTransformer(TransformToSpecsNodeTransformer):
+
+    def _context_expr_for(self, node):
+        return node.items[0].context_expr
+
+    def _generate_self(self):
+        return ast.arguments(
+            args=[ast.arg(arg='self', annotation=None)],
+            vararg=None,
+            kwonlyargs=[],
+            kw_defaults=[],
+            kwarg=None,
+            defaults=[]
         )
 
