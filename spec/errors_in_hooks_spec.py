2af0adb5b769d2e7a38afdce7e650c60e3b214fc
Néstor Salceda <nestor.salceda@gmail.com>
2014-11-19 16:33:08 +0100

    Added examples for before.all hooks

--- spec/errors_in_hooks_spec.py
+++ spec/errors_in_hooks_spec.py
@@ -6,11 +6,43 @@ from mamba import reporter
 from spec.object_mother import *
 
 with description('Errors in hooks'):
+    with before.each:
+        self.reporter = Spy(reporter.Reporter)
+        self.example_group = an_example_group()
+
+    with context('when an error was raised in a before.all hook'):
+        with before.each:
+            self.example_group.hooks['before_all'].append(self._error)
+
+        with it('marks example as failed'):
+            self.example = an_example()
+            self.example_group.append(self.example)
+
+            self.example_group.run(self.reporter)
+
+            expect(self.example.error).not_to(be_none)
+
+        with context('when example also launches an error'):
+            with context('when before.each also launches an error'):
+                with it('keeps the error happened in first hook'):
+                    self.example_group.hooks['before_each'].append(self._other_error)
+                    self.example = a_failing_example()
+                    self.example_group.append(self.example)
+
+                    self.example_group.run(self.reporter)
+
+                    expect(isinstance(self.example.error.exception, NotImplementedError)).to(be_true)
+
+            with it('keeps the error happened in hook'):
+                self.example = a_failing_example()
+                self.example_group.append(self.example)
+
+                self.example_group.run(self.reporter)
+
+                expect(isinstance(self.example.error.exception, NotImplementedError)).to(be_true)
 
     with context('when an error was raised in a before.each hook'):
         with before.each:
-            self.reporter = Spy(reporter.Reporter)
-            self.example_group = an_example_group()
             self.example_group.hooks['before_each'].append(self._error)
 
         with it('marks example as failed'):
@@ -30,7 +62,9 @@ with description('Errors in hooks'):
 
                 expect(isinstance(self.example.error.exception, NotImplementedError)).to(be_true)
 
-        def _error(self):
-            raise NotImplementedError()
 
+    def _error(self):
+        raise NotImplementedError()
 
+    def _other_error(self):
+        raise IOError()
