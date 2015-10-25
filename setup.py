d1c3dc5b0f230bce37e6fd49e540536edc533120
Elias Dorneles <eliasdorneles@gmail.com>
2015-10-25 18:49:21 -0200

    declare minimal dependencies in setup.py install_requires

--- setup.py
+++ setup.py
@@ -25,7 +25,11 @@ setup(name='mamba',
       packages=find_packages(exclude=['ez_setup', 'examples', 'spec', 'spec.*']),
       include_package_data=True,
       zip_safe=False,
-      install_requires=[line for line in open('requirements.txt')],
+      install_requires=[
+          'clint',
+          'coverage',
+          'watchdog'
+      ],
       entry_points={
           'console_scripts': [
               'mamba = mamba.cli:main'
