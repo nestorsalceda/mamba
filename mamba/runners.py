4ecc6b4dea25e2c2f86495fed44b492c3f97ddf6
Ángel Sanz <angelsanzgit@gmail.com>
2015-12-17 00:19:30 +0000

    Runners: mark all instance attributes as private

--- mamba/runners.py
+++ mamba/runners.py
@@ -16,22 +16,22 @@ class Runner(object):
 class BaseRunner(Runner):
 
     def __init__(self, example_collector, loader, reporter):
-        self.example_collector = example_collector
-        self.loader = loader
-        self.reporter = reporter
+        self._example_collector = example_collector
+        self._loader = loader
+        self._reporter = reporter
         self._has_failed_examples = False
 
     def run(self):
-        self.reporter.start()
+        self._reporter.start()
 
-        for module in self.example_collector.modules():
+        for module in self._example_collector.modules():
             self._run_examples_in(module)
 
-        self.reporter.finish()
+        self._reporter.finish()
 
     def _run_examples_in(self, module):
-        for example_group in self.loader.load_examples_from(module):
-            example_group.run(self.reporter)
+        for example_group in self._loader.load_examples_from(module):
+            example_group.run(self._reporter)
 
             if example_group.failed:
                 self._has_failed_examples = True
@@ -44,24 +44,24 @@ class BaseRunner(Runner):
 class CodeCoverageRunner(Runner):
 
     def __init__(self, runner):
-        self.runner = runner
+        self._runner = runner
 
     def run(self):
         with code_coverage.CodeCoverage():
-            self.runner.run()
+            self._runner.run()
 
     @property
     def has_failed_examples(self):
-        return self.runner.has_failed_examples
+        return self._runner.has_failed_examples
 
 
 class FileWatcherRunner(Runner):
 
     def __init__(self, runner):
-        self.file_watcher = file_watcher.FileWatcher(runner)
+        self._file_watcher = file_watcher.FileWatcher(runner)
 
     def run(self):
-        self.file_watcher.wait_for_events()
+        self._file_watcher.wait_for_events()
 
     @property
     def has_failed_examples(self):
