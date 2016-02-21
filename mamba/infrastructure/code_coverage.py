baf2be0db2d556e2ef4d188c93342520fc5cebaa
Eduardo Ferro Aldama <eferro@themotion.com>
2016-02-21 23:51:20 +0000

    coverage output file configurable

--- mamba/infrastructure/code_coverage.py
+++ mamba/infrastructure/code_coverage.py
@@ -5,8 +5,8 @@ import coverage
 
 class CodeCoverage(object):
 
-    def __init__(self):
-        self.coverage = coverage.coverage()
+    def __init__(self, code_coverage_file):
+        self.coverage = coverage.coverage(data_file=code_coverage_file)
 
     def __enter__(self):
         self.coverage.start()
