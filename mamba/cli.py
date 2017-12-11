32215119bbd60790a109c39ad1dac6ed4e25bb5d
Néstor Salceda <nestor.salceda@gmail.com>
2017-12-11 16:05:31 +0100

    Add documentation for tags feature

--- mamba/cli.py
+++ mamba/cli.py
@@ -24,14 +24,14 @@ def main():
 def _parse_arguments():
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('--version', '-v', default=False, action='store_true', help='Display the version')
-    parser.add_argument('--slow', '-s', default=0.075, type=float, help='Slow test threshold in seconds (default: %(default)s)')
-    parser.add_argument('--enable-coverage', default=False, action='store_true', help='Enable code coverage measurement (default: %(default)s)')
-    parser.add_argument('--coverage-file', default='.coverage', action='store', help='Name of coverage data file (default: %(default)s)')
-    parser.add_argument('--format', '-f', default='progress', action='store', choices=['documentation', 'progress'], help='Output format (default: %(default)s)')
-    parser.add_argument('specs', default=['./spec', './specs'], nargs='*', help='Paths to specs to run or directories with specs to run (default: %(default)s)')
-    parser.add_argument('--no-color', default=False, action='store_true', help='Turn off all output coloring (default: %(default)s)')
-    parser.add_argument('--tags', '-t', default=None, type=lambda x: [tag.strip() for tag in x.split(',')], action='store')
+    parser.add_argument('--version', '-v', default=False, action='store_true', help='display the version')
+    parser.add_argument('--slow', '-s', default=0.075, type=float, help='slow test threshold in seconds (default: %(default)s)')
+    parser.add_argument('--enable-coverage', default=False, action='store_true', help='enable code coverage measurement (default: %(default)s)')
+    parser.add_argument('--coverage-file', default='.coverage', action='store', help='name of coverage data file (default: %(default)s)')
+    parser.add_argument('--format', '-f', default='progress', action='store', choices=['documentation', 'progress'], help='output format (default: %(default)s)')
+    parser.add_argument('specs', default=['./spec', './specs'], nargs='*', help='paths to specs to run or directories with specs to run (default: %(default)s)')
+    parser.add_argument('--no-color', default=False, action='store_true', help='turn off all output coloring (default: %(default)s)')
+    parser.add_argument('--tags', '-t', default=None, type=lambda x: [tag.strip() for tag in x.split(',')], action='store', help='run examples with specified tags (example: -t unit,integration)')
 
     return parser.parse_args()
 
