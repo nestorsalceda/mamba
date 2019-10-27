bb920482ca7d1227ffaea1f0668633043eb184af
Marcelo Manzan <manzan@gmail.com>
2019-10-27 12:46:49 -0300

    lazy_spec.py :: cleanup

--- spec/lazy_spec.py
+++ spec/lazy_spec.py
@@ -1,14 +1,14 @@
 from mamba import description, it, context, lazy
-from expects import expect, be_true, be_false, equal
+from expects import expect, equal
 
 with description('Lazy'):
 
     with lazy.my_awesome_dict as self:
-        # print('the my_awesome_dict method should be called once')
+        # print('the my_awesome_dict method should be called just once')
         self._my_awesome_dict = dict(number=self.my_awesome_number())
 
     with lazy.my_awesome_number as self:
-        # print('the my_awesome_number1 method should be called once')
+        # print('the my_awesome_number1 method should be called just once')
         self._my_awesome_number = 1
 
     with it('should be equal') as self:
@@ -17,7 +17,7 @@ with description('Lazy'):
 
     with context('sub context'):
         with lazy.my_awesome_number as self:
-            # print('the my_awesome_number2 method should be called once')
+            # print('the my_awesome_number2 method should be called just once')
             self._my_awesome_number = 2
 
         with it('should be equal') as self:
