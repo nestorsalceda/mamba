9f37f8eff08480108113b28487009ef9f717dbd8
Marcelo Manzan <manzan@gmail.com>
2019-10-27 13:36:31 -0300

    lazy_spec.py :: check calls_count to be once

--- spec/lazy_spec.py
+++ spec/lazy_spec.py
@@ -1,25 +1,33 @@
-from mamba import description, it, context, lazy
+from mamba import description, it, context, before, lazy
 from expects import expect, equal
 
 with description('Lazy'):
 
+    with before.each as self:
+        self.lazy_calls_count = 0
+
     with lazy.my_awesome_dict as self:
-        # print('the my_awesome_dict method should be called just once')
         self._my_awesome_dict = dict(number=self.my_awesome_number())
 
     with lazy.my_awesome_number as self:
-        # print('the my_awesome_number1 method should be called just once')
+        self.lazy_calls_count = self.lazy_calls_count + 1
         self._my_awesome_number = 1
 
-    with it('should be equal') as self:
+    with it('should call once') as self:
+        expect(self.lazy_calls_count).to(equal(0))
         expect(self.my_awesome_number()).to(equal(1))
+        expect(self.lazy_calls_count).to(equal(1))
         expect(self.my_awesome_dict()).to(equal(dict(number=1)))
+        expect(self.lazy_calls_count).to(equal(1))
 
     with context('sub context'):
         with lazy.my_awesome_number as self:
-            # print('the my_awesome_number2 method should be called just once')
+            self.lazy_calls_count = self.lazy_calls_count + 1
             self._my_awesome_number = 2
 
-        with it('should be equal') as self:
+        with it('should call once') as self:
+            expect(self.lazy_calls_count).to(equal(0))
             expect(self.my_awesome_number()).to(equal(2))
+            expect(self.lazy_calls_count).to(equal(1))
             expect(self.my_awesome_dict()).to(equal(dict(number=2)))
+            expect(self.lazy_calls_count).to(equal(1))
